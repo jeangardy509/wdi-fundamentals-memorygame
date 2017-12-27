42eb1cc873b7e6517f1d128ebb3fbbecdf659df9
Jean Mompremier <jeangardy509@gmail.com>
2017-12-27 14:00:00 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -9,9 +30,10 @@ var checkForMatch = function() {
 }
 };
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId]);
-console.log("user flipped " + cards[cardId]);
-console.log("user flipped " + cards[cardId]);
+	console.log("user flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 
 flipCard(0);
