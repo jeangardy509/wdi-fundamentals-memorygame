8bff58baff31d31541594749b7f7569a726febee
Jean Mompremier <jeangardy509@gmail.com>
2017-12-26 19:25:57 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,7 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = [0];
+var cardTwo = [2];
+
+cardsInPlay.push("cardOne");
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped queen");
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	alert("you have found a match!");
+}
+else {
+	alert("sorry, try again!");
+}
